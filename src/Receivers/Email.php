c38cc100a7f889f9e57967e6a9c43a2258f692a0
Sean Kennedy <sean@kndy.net>
2019-04-18 16:13:06 -0600

    Email receiver now supports subject/msg templates

--- src/Receivers/Email.php
+++ src/Receivers/Email.php
@@ -55,11 +55,8 @@ class Email extends AbstractReceiver
         echo "firing email to {$this->emailAddress}\n";
         $env = $this->config;
         $env['to'] = $this->emailAddress;
-        $env['subject'] = $alert->isRecovered() ? 'RECOVERED' : 'DOWN';
-        $env['message'] = '';
-        foreach ($alert->getAttributes() as $key => $val) {
-            $env['message'].= "$key: $val\n";
-        }
+        $env['subject'] = $this->interpolate($alert->getAttributes(), $this->subjectTemplate);
+        $env['message'] = $this->interpolate($alert->getAttributes(), $this->messageTemplate);
 
         $process = new Process('php '.__DIR__.'/../../bin/send-email.php', null, $env);
         $process->start($this->loop);
@@ -74,5 +71,26 @@ class Email extends AbstractReceiver
             $deferred->resolve($exitCode);
         });
         return $deferred->promise();
+    }
+
+    /**
+     * Interpolate values from $vars [variable=>value] into $str
+     * where $str uses %foo% for variable named 'foo' from $vars.
+     * Case insensitive.
+     *
+     * @var array $vars Array of variable=>value pairs used as interpolation source
+     * @var string $str Template string using variables from $vars as %var%
+     *
+     * @return string The interpolated string
+     */
+    private function interpolate(array $vars, string $str)
+    {
+        return \str_ireplace(
+            \array_map(function ($var) {
+                return "%$var%";
+            }, \array_keys($vars)),
+            \array_values($vars),
+            $str
+        );
     }
 }
