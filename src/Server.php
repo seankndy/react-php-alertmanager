de09b7fca31f3ca160a9baa0ac431f86bae64724
Sean Kennedy <sean@kndy.net>
2019-05-07 10:22:07 -0600

    add Server as the eventemitter for Alerts API class

--- src/Server.php
+++ src/Server.php
@@ -55,7 +55,7 @@ class Server extends EventEmitter
         $socket = new SocketServer($listen, $this->loop);
         $this->httpServer->listen($socket);
 
-        $alertsApi = new \Api\V1\Alerts($queue);
+        $alertsApi = new \Api\V1\Alerts($queue, $this);
         $this->httpDispatcher = \FastRoute\simpleDispatcher(
             function(\FastRoute\RouteCollector $r) use ($alertsApi) {
                 $r->addGroup('/api/v1', function (\FastRoute\RouteCollector $r) {
