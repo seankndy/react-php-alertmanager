15e1c4ac14c439db5ea73c865845641f884b68f3
Sean Kennedy <sean@kndy.net>
2019-05-11 17:36:30 -0600

    remove now unneeded interpolate() method from Slack

--- src/Receivers/Slack.php
+++ src/Receivers/Slack.php
@@ -128,27 +128,6 @@ class Slack extends AbstractReceiver
     }
 
     /**
-     * Interpolate values from $vars [variable=>value] into $str
-     * where $str uses %foo% for variable named 'foo' from $vars.
-     * Case insensitive.
-     *
-     * @var array $vars Array of variable=>value pairs used as interpolation source
-     * @var string $str Template string using variables from $vars as %var%
-     *
-     * @return string The interpolated string
-     */
-    private function interpolate(array $vars, string $str)
-    {
-        return \str_ireplace(
-            \array_map(function ($var) {
-                return "%$var%";
-            }, \array_keys($vars)),
-            \array_values($vars),
-            $str
-        );
-    }
-
-    /**
      * Get the memberId
      *
      * @return string
