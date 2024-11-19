fdc4b03e648684bfb1235eb3b91b298b744b7b3c
Sean Kennedy <sean@kndy.net>
2024-11-19 09:34:28 -0700

    remove deprecated swiftmailer, move to symfony mailer

--- bin/send-email.php
+++ bin/send-email.php
@@ -6,9 +6,15 @@
  *
  */
 
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
+use Symfony\Component\Mime\Email;
+
 $autoload = [
     __DIR__.'/../../../../vendor/autoload.php',
     __DIR__.'../../vendor/autoload.php',
+    __DIR__.'/../vendor/autoload.php',
 ];
 foreach ($autoload as $file) {
     if (\file_exists($file)) {
@@ -19,21 +25,28 @@ foreach ($autoload as $file) {
 
 if (!isset($_SERVER['server'], $_SERVER['port'], $_SERVER['from'],
     $_SERVER['to'], $_SERVER['message'])) {
+    fprintf(STDERR, "Required environment variables missing.");
     exit(1);
 }
 $to = preg_split('/[,;]\s*/', $_SERVER['to']);
 
-$transport = new Swift_SmtpTransport($_SERVER['server'], $_SERVER['port']);
+$transport = new EsmtpTransport($_SERVER['server'], $_SERVER['port']);
 if (isset($_SERVER['username'], $_SERVER['password'])) {
     $transport->setUsername($_SERVER['username']);
     $transport->setPassword($_SERVER['password']);
 }
 
-$mailer = new Swift_Mailer($transport);
+$mailer = new Mailer($transport);
 
-$message = (new Swift_Message(isset($_SERVER['subject']) ? $_SERVER['subject'] : ''))
-    ->setFrom([$_SERVER['from']])
-    ->setTo($to)
-    ->setBody($_SERVER['message']);
+$message = (new Email())
+    ->from($_SERVER['from'])
+    ->to(...$to)
+    ->subject($_SERVER['subject'] ?? '')
+    ->text($_SERVER['message']);
 
-$mailer->send($message);
+try {
+    $mailer->send($message);
+} catch (TransportExceptionInterface $e) {
+    fprintf(STDERR, "Failed sending email: %s", $e->getMessage());
+    exit(1);
+}
